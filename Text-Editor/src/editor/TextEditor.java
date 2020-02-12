778d52d3130bbea13447352b193314773c27a563
Jonathan Jones <kjjejones42@gmail.com>
2020-02-12 11:41:40 +0000

    added file reading functionality

--- Text-Editor/src/editor/TextEditor.java
+++ Text-Editor/src/editor/TextEditor.java
@@ -2,31 +2,74 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.io.*;
+import java.nio.file.*;
 
 public class TextEditor extends JFrame {
 
     static final long serialVersionUID = 1;
 
+    private static String readLineByLineJava8(File filePath) {
+        StringBuilder contentBuilder = new StringBuilder();
+        byte[] barr = new byte[1];
+        try {
+            byte[] bytes = Files.readAllBytes(filePath.toPath());
+            for (byte b : bytes) {
+                try {
+                    barr[0] = b;
+                    contentBuilder.append(new String(barr));
+                } catch (Exception e) {
+                    contentBuilder.append(" ");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
+    }
+
     public TextEditor() {
         super("Swing Example");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-
+        JPanel center = new JPanel();
+        center.setLayout(new BorderLayout());
+        center.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         JTextArea textArea = new JTextArea();
+
+        JPanel textAreaContainer = new JPanel();
+        textAreaContainer.setLayout(new BorderLayout());
+        textAreaContainer.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        textAreaContainer.add(textArea, BorderLayout.CENTER);
+        textAreaContainer.setBackground(Color.WHITE);
+
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
-        textArea.setName("TextArea");
-        textArea.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+        JScrollPane pane = new JScrollPane(textAreaContainer);
+        center.add(pane);
+        add(center, BorderLayout.CENTER);
 
-        panel.add(textArea);
-        add(panel, BorderLayout.CENTER);
+        textArea.setName("TextArea");
+        pane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+        JPanel top = new JPanel();
+        center.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        JButton button = new JButton("Choose File");
+        JFileChooser chooser = new JFileChooser();
+        button.addActionListener(l -> {
+            if (chooser.showOpenDialog(button) == JFileChooser.APPROVE_OPTION) {
+                textArea.setText(readLineByLineJava8(chooser.getSelectedFile()));
+            }
+        });
+        top.add(button);
+        add(top, BorderLayout.NORTH);
 
         setVisible(true);
     }
